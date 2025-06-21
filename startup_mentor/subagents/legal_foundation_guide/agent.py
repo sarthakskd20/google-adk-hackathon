e638a80aa8d7dac4212809770c435d69c70dce07
Sarthak Dhonde <85759905+sarthakskd20@users.noreply.github.com>
2025-06-22 03:45:32 +0530

    Update agent.py

--- startup_mentor/subagents/legal_foundation_guide/agent.py
+++ startup_mentor/subagents/legal_foundation_guide/agent.py
@@ -1,12 +1,13 @@
-from google.adk.agents import LlmAgent
+from google.adk.agents import Agent
 from google.adk.tools import google_search
 
 from . import prompt 
-MODEL = "gemini-2.5-pro-preview-05-06" 
+MODEL = "gemini-2.5-pro" 
 
-legal_foundation_guide_agent = LlmAgent(
+legal_foundation_guide_agent = Agent(
     name="legal_foundation_guide_agent",
     model=MODEL,
+    description="It guides the user with the legal documents and awares user with the legal guidance required for their startup.",
     instruction=prompt.LEGAL_FOUNDATION_GUIDE_PROMPT,
     output_key="legal_foundation_report", 
     tools=[google_search], 
