e2e8b3bbf968acea176660d2e3fe0c258223c006
Sarthak Dhonde <85759905+sarthakskd20@users.noreply.github.com>
2025-06-22 03:46:55 +0530

    Update agent.py

--- startup_mentor/subagents/current_opportunity_finder/agent.py
+++ startup_mentor/subagents/current_opportunity_finder/agent.py
@@ -1,13 +1,14 @@
-from google.adk.agents import LlmAgent
+from google.adk.agents import Agent
 from google.adk.tools import google_search
 
 from . import prompt
 
-MODEL = "gemini-2.5-pro-preview-05-06"  
+MODEL = "gemini-2.5-pro"  
 
-current_opportunity_finder_agent = LlmAgent(
-    name="idea_validation_engine_agent",
+current_opportunity_finder_agent = Agent(
+    name="current_opportunity_finder_agent",
     model=MODEL,
+    description="It searches the with the help of google search and finds the current available opportunities for their startup journey and guides the user.",
     instruction=prompt.CURRENT_OPPORTUNITY_FINDING_PROMPT,
     output_key="current_opportunities_finder-output",
     tools=[google_search],
