d9a8ced7d057e6cafb9c12b9d85f1dde73e2dfb1
Sarthak Dhonde <85759905+sarthakskd20@users.noreply.github.com>
2025-06-22 03:41:50 +0530

    Update agent.py

--- startup_mentor/subagents/tech_stack_advisor/agent.py
+++ startup_mentor/subagents/tech_stack_advisor/agent.py
@@ -1,12 +1,16 @@
-from google.adk.agents import LlmAgent
+
+from google.adk.agents import Agent
+
 from google.adk.tools import google_search
 from . import prompt
 
-MODEL = "gemini-2.5-pro-preview-05-06"
+MODEL = "gemini-2.5-pro"
 
-tech_stack_advisor_agent = LlmAgent(
+
+tech_stack_advisor_agent = Agent(
     name="tech_stack_advisor_agent",
     model=MODEL,
+    description="It will provide the user personalized, actionable technology recommendations.",
     instruction=prompt.TECH_STACK_ADVISOR_PROMPT,
     output_key="tech_stack_report",
     tools=[google_search],
