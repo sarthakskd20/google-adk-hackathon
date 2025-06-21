d07c9f04302f7042997620108bcb289b0c9014c4
Sarthak Dhonde <85759905+sarthakskd20@users.noreply.github.com>
2025-06-22 03:46:10 +0530

    Update agent.py

--- startup_mentor/subagents/idea_validation_engine/agent.py
+++ startup_mentor/subagents/idea_validation_engine/agent.py
@@ -1,13 +1,14 @@
-from google.adk.agents import LlmAgent
+from google.adk.agents import Agent
 from google.adk.tools import google_search
 
 from . import prompt
 
-MODEL = "gemini-2.5-pro-preview-05-06" 
+MODEL = "gemini-2.5-pro" 
 
-idea_validation_engine_agent = LlmAgent(
+idea_validation_engine_agent = Agent(
     name="idea_validation_engine_agent",
     model=MODEL,
+    description="analyzes the startup_idea of the user, analyzes it, and guides the user for the improvements and judges it whether it is a good idea or not",
     instruction=prompt.IDEA_VALIDATION_ENGINE_PROMPT,
     output_key="idea_validation_report",
     tools=[google_search],
