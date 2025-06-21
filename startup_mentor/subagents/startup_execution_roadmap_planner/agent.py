60db0b94564be09746575790418bd587cc246f74
Sarthak Dhonde <85759905+sarthakskd20@users.noreply.github.com>
2025-06-22 03:42:30 +0530

    Update agent.py

--- startup_mentor/subagents/startup_execution_roadmap_planner/agent.py
+++ startup_mentor/subagents/startup_execution_roadmap_planner/agent.py
@@ -1,12 +1,14 @@
-from google.adk.agents import LlmAgent
+from google.adk.agents import Agent
 from google.adk.tools import google_search 
+
 from . import prompt 
 
-MODEL = "gemini-2.5-pro-preview-05-06" 
+MODEL = "gemini-2.5-pro" 
 
-startup_execution_roadmap_planner_agent = LlmAgent(
+startup_execution_roadmap_planner_agent = Agent(
     name="startup_execution_roadmap_planner_agent",
     model=MODEL,
+    description="This will design a practical roadmap for the user for his startup journey.",
     instruction=prompt.STARTUP_EXECUTION_ROADMAP_PLANNER_PROMPT,
     output_key="startup_roadmap_report", 
     tools=[google_search], 
