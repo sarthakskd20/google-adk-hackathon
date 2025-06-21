851c3c0559f0adbe3f15e70e4cade98d183e034b
Sarthak Dhonde <85759905+sarthakskd20@users.noreply.github.com>
2025-06-21 21:32:22 +0530

    Update agent.py

--- startup_mentor/subagents/personalized_financial_architect/agent.py
+++ startup_mentor/subagents/personalized_financial_architect/agent.py
@@ -2,7 +2,7 @@ from google.adk.agents import LlmAgent
 from google.adk.tools import google_search
 from . import prompt 
 
-MODEL = "gemini-2.5-pro" 
+MODEL = "gemini-2.5-pro-preview-05-06" 
 
 personalized_financial_architect_agent = LlmAgent(
     name="personalized_financial_architect_agent",
