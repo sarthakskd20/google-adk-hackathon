bfc1eb20f3d028cdd8bd7ac212c915b2a245ca95
Sarthak Dhonde <85759905+sarthakskd20@users.noreply.github.com>
2025-06-22 03:44:50 +0530

    Update agent.py

--- startup_mentor/subagents/market_insight_strategist/agent.py
+++ startup_mentor/subagents/market_insight_strategist/agent.py
@@ -1,13 +1,15 @@
-from google.adk.agents import LlmAgent
+from google.adk.agents import Agent
 from google.adk.tools import google_search 
+
 
 from . import prompt 
 
-MODEL = "gemini-2.5-pro-preview-05-06" 
+MODEL = "gemini-2.5-pro" 
 
-market_insight_strategist_agent = LlmAgent(
+market_insight_strategist_agent = Agent(
     name="market_insight_strategist_agent",
     model=MODEL,
+    description="analyzes the market insight and strategises things and helps user for their startup by the recommending the additions and improvements with the current market states.",
     instruction=prompt.MARKET_INSIGHT_STRATEGIST_PROMPT,
     output_key="market_insight_report", 
     tools=[google_search], 
